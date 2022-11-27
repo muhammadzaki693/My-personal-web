6785cd6f8d61ca8cb174090fea8223c9958db19c
Muhammadzakirha <75458189+muhammadzaki693@users.noreply.github.com>
2022-11-27 09:52:21 +0000

    Smoll

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, url_for, request
-from flask_autoindex import AutoIndex
+from flask import Flask, render_template, url_for, request, abort, send_file
+from hurry.filesize import size, alternative
 import os
+from datetime import datetime
 port = os.environ.get('PORT', 8080)
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-AutoIndex(app, browse_root=os.path.curdir)
 
 @app.template_filter('autoversion')
 def autoversion_filter(filename):
@@ -20,5 +20,17 @@ def autoversion_filter(filename):
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route("/files/",defaults={"reqpath": ""})
+@app.route("/files/<path:reqpath>")
+def files(reqpath):
+	BASE_DIR = os.path.curdir
+	abs_path = os.path.join(BASE_DIR, reqpath)
+	if not os.path.exists(abs_path):
+		return abort(404)
+	if os.path.isfile(abs_path):
+		return send_file(abs_path)
+	files = os.listdir(abs_path)
+	return render_template("files.html",files=files,getmtime=os.path.getmtime,getctime=os.path.getctime,size=size,alternative=alternative,getsize=os.path.getsize,path=os.path.curdir,fromtimestamp=datetime.fromtimestamp)
 
 app.run(host='0.0.0.0', port=port)
