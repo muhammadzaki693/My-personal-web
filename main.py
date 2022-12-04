73fe31bac676ef4555e475990ab6364b336232ff
Muhammadzakirha <75458189+muhammadzaki693@users.noreply.github.com>
2022-12-04 04:16:02 +0000

    Changed everything

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-from flask import Flask, render_template, url_for, request, abort, send_file
-from hurry.filesize import size, alternative
-from werkzeug import secure_filename
 import os
+import replit
+import glob
+import json
+import sys
+import markdown
+import markdown.extensions.fenced_code
+import markdown.extensions.codehilite
+import pygments
+from replit import db
 from datetime import datetime
+from flask import Flask, render_template, url_for, request, jsonify
+from pygments.formatters import HtmlFormatter
+
 port = os.environ.get('PORT', 8080)
+
+for apifile in glob.glob("api/*.json"):
+	with open(apifile) as f:
+		globals()[apifile.rsplit("/",1)[1].rsplit(".",1)[0]] = json.load(f)
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -22,23 +35,23 @@ def autoversion_filter(filename):
 def index():
     return render_template('index.html')
 
-@app.route("/files/",defaults={"reqpath": ""})
-@app.route("/files/<path:reqpath>")
-def files(reqpath):
-	BASE_DIR = os.path.curdir
-	abs_path = os.path.join(BASE_DIR, reqpath)
-	if not os.path.exists(abs_path):
-		return abort(404)
-	if os.path.isfile(abs_path):
-		return send_file(abs_path)
-	files = os.listdir(abs_path)
-	return render_template("files.html",files=files,getmtime=os.path.getmtime,getctime=os.path.getctime,size=size,alternative=alternative,getsize=os.path.getsize,path=os.path.curdir,fromtimestamp=datetime.fromtimestamp)
+@app.route("/api", defaults={"query":""})
+@app.route('/api/<string:query>')
+def api(query):
+	if query == "":
+		return jsonify({"error":"you didnt provide any api"})
+	return jsonify(globals()[query]) if query in globals() else jsonify({"error":"api doesnt exist"})
 
-@app.route("/uploader", methods=["GET","POST"])
-def upload():
-	if request.method == "POST":
-		f = secure_filename(file.filename)
-		f.save(os.path.join(request.args.get("dir"),f))
-		return "success"
+@app.route("/zakipy")
+def zakipy():
+	formatter = HtmlFormatter(style="emacs",full=True,cssclass="codehilite")
+	css_string = formatter.get_style_defs()
+	md_css_string = "<style>"+css_string+"</style>"
+	readme_file = open("templates/README_ZAKIPY.md","r")
+	readme_string = markdown.markdown(
+		readme_file.read(),extensions=["fenced_code","codehilite"]
+	)
+	readme_template = md_css_string + readme_string
+	return readme_template
 
 app.run(host='0.0.0.0', port=port)
---
+++ templates/README_ZAKIPY.md
@@ -0,0 +1,23 @@
+# ZAKIPY
+
+welcome fellas from every where on earth whe are going to review my ien package an 3 2 1 lets go
+> this is still in development
+
+## installation
+```python
+#install part
+pip install zakipy
+
+#import in python
+import zakipy
+```
+
+## modules
+
+1. converter:  
+   its just a basic converter not to much it can do some basic stuff like doing chiper and convert base. to import this module just type `from zakipy.Converter import *` now you can access all the function and module
+2. math:
+   a basic math it only had 2 function wich is fibbonaci, and prime to import use the following word `from zakipy.Math import *`
+
+## end
+thanks for reading this because i work hard for like 6 hour to render it thank you maybe you could support us with subscribe my youtube channel "MzakyR"
